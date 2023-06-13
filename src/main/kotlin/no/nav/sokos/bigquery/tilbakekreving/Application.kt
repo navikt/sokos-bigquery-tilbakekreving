61dc535ce9c89c60a695cfb5e3b73a75a8b3670b
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2023-06-13 09:01:04 +0200

    Glemte å trigge fetchinga...

--- src/main/kotlin/no/nav/sokos/bigquery/tilbakekreving/Application.kt
+++ src/main/kotlin/no/nav/sokos/bigquery/tilbakekreving/Application.kt
@@ -3,5 +3,5 @@ package no.nav.sokos.bigquery.tilbakekreving
 import no.nav.sokos.bigquery.tilbakekreving.service.DataFetcherService
 
 fun main() {
-    //DataFetcherService().fetch()
+    DataFetcherService().fetch()
 }
